b76d607594af4bc6c2c77c9e552d458b5125ca8d
Frederico De Paoli <frederico.de.paoli@gmail.com>
2017-03-20 18:53:48 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,25 @@
 
+class CrewMember {
+  constructor(position) {
+    this.position = position
+    this.currentShip = 'Looking for a Rig'
+  }
+  engageWarpDrive() {
+    if (this.position === 'Pilot' && typeof this.currentShip === 'object') {
+      this.currentShip.warpDrive = 'engaged!'
+    }
+    return 'had no effect'
+  }
+  setsInvisibility() {
+    if (this.position === 'Defender' && typeof this.currentShip === 'object') {
+      this.currentShip.cloaked = true
+    }
+    return 'had no effect'
+  }
+  chargePhasers() {
+    if (this.position === 'Gunner' && typeof this.currentShip === 'object') {
+      this.currentShip.phasersCharge = 'charged!'
+    }
+    return 'had no effect'
+  }
+}
